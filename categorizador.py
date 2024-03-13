322e2ceb631b47cba31baf6ad37a906a7c1fe9a3
joaodamásio <joaovitormessiasdamasio@gmail.com>
2024-03-13 17:09:28 -0300

    criando funcao de categorizacao de produtos, usando um prompt template para inserir dinamicamente parametros como categorias validas e nome do produto, permitindo uma interacao mais flexivel e organizada com o codigo

--- categorizador.py
+++ categorizador.py
@@ -6,44 +6,47 @@ load_dotenv()
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-3.5-turbo"
 
+def categoriza_produto(nome_produto, lista_categorias_possiveis):
+    prompt_sistema = f"""
+            Você é um categorizador de produtos.
+            Você deve assumir as categorias presentes na lista abaixo.
 
-prompt_sistema = f"""
-        Você é um categorizador de produtos.
-        Você deve assumir as categorias presentes na lista abaixo.
+            # Lista de Categorias Válidas
+            {lista_categorias_possiveis.split(",")}
 
-        # Lista de Categorias Válidas
-            - Moda Sustentável
-            - Produtos para o Lar
-            - Beleza Natural
-            - Eletrônicos Verdes
-            - Higiene Pessoal
+            # Formato da Saída
+            Produto: Nome do Produto
+            Categoria: apresente a categoria do produto
 
-        # Formato da Saída
-        Produto: Nome do Produto
-        Categoria: apresente a categoria do produto
+            # Exemplo de Saída
+            Produto: Escova elétrica com recarga solar
+            Categoria: Eletrônicos Verdes
+        """
 
-        # Exemplo de Saída
-        Produto: Escova elétrica com recarga solar
-        Categoria: Eletrônicos Verdes
-    """
+
+    resposta = cliente.chat.completions.create(
+        messages=[
+            {
+                "role":"system",
+                "content": prompt_sistema
+            },
+            {
+                "role": "user",
+                "content" : nome_produto
+            }
+            
+        ],
+        model= modelo,
+        temperature= 0,
+        max_tokens=200,
+    )
     
-prompt_usuario = input("Apresente o nome de um produto: ")
+    return resposta.choices[0].message.content
 
-resposta = cliente.chat.completions.create(
-    messages=[
-        {
-            "role":"system",
-            "content": prompt_sistema
-        },
-        {
-            "role": "user",
-            "content" : prompt_usuario
-        }
-    ],
-    model= modelo,
-    temperature= 0,
-    max_tokens=200,
-)
 
-print(resposta.choices[0].message.content)
-    
+categorias_validas = input("Informe as categorias validas, separando por vírgula: ")
+
+while True:
+    nome_produto = input("Digite o nome do produto:")
+    texto_resposta = categoriza_produto(nome_produto, categorias_validas)
+    print(texto_resposta)
